15794bf4c00f45409cd2f8431fd5fb846f816fb9
Yoni Romm <yonatan.romm@sap.com>
2022-05-20 12:22:49 +0300

    add project

---
+++ c/simple.sh
@@ -0,0 +1,5 @@
+gcc -ansi -Wall -Wextra -Werror -pedantic-errors kmeans.c -lm -o hw1
+./hw1 3 600 testFiles/input1.txt output1.txt
+./hw1 7 testFiles/input_2.txt output2.txt
+./hw1 15 testFiles/input_3.txt output3.txt
+
---
+++ python/simple.sh
@@ -0,0 +1,3 @@
+python3 kmeans.py 3 600 testFiles/input1.txt output1.txt
+python3 kmeans.py 7 testFiles/input_2.txt output2.txt
+python3 kmeans.py 15 testFiles/input_3.txt output3.txt
